ee71b6c6be380486d9011848558dac04c5da6dad
CrashStatic <thecrashstatic@gmail.com>
2025-02-17 21:41:26 +0300

    Добавляет маску телефона в компонент формы

--- src/UI/input/InputField.tsx
+++ src/UI/input/InputField.tsx
@@ -1,16 +1,50 @@
 import './InputField.css';
+import React, {useState} from "react";
 
 interface InputFieldProps {
   id: string;
   label: string;
   placeholder: string;
+  type: 'phone' | 'text';
 }
 
-export default function InputField({id, label, placeholder}: InputFieldProps) {
+export default function InputField({id, label, placeholder, type}: InputFieldProps) {
+  const [value, setValue] = useState('');
+
+  function handleInputChange(e: React.ChangeEvent<HTMLInputElement>) {
+    let inputValue = e.target.value;
+
+    if (type === 'phone') {
+      inputValue = inputValue.replace(/\D/g, '');
+
+      if (inputValue.length === 1 && inputValue[0] !== '7') {
+        inputValue = `7${inputValue}`;
+      }
+
+      let formattedValue = '+7 ';
+
+      if (inputValue.length > 1) formattedValue += inputValue.substring(1, 4);
+      if (inputValue.length > 4) formattedValue += ` ${inputValue.substring(4, 7)}`;
+      if (inputValue.length > 7) formattedValue += ` ${inputValue.substring(7, 9)}`;
+      if (inputValue.length > 9) formattedValue += ` ${inputValue.substring(9, 11)}`;
+
+      setValue(formattedValue);
+    } else {
+      setValue(inputValue);
+    }
+  }
+
   return (
     <div className="wrapper">
       <label className="label" htmlFor={id}>{label}</label>
-      <input className="input" id={id} name={id} type="text" placeholder={placeholder} />
+      <input
+        className="input"
+        id={id} name={id}
+        type="text"
+        value={value}
+        placeholder={placeholder}
+        onChange={handleInputChange}
+      />
     </div>
   )
 }
--- src/components/addContactForm/AddContactForm.tsx
+++ src/components/addContactForm/AddContactForm.tsx
@@ -6,23 +6,9 @@ export default function AddContactForm() {
   return (
     <form className="interaction__form form" action="#" method="post" name="contact-add">
       <div className="form__inputs">
-        <InputField id='name' label='Name' placeholder='Ivan' />
-        <InputField id='position' label='Position' placeholder='Developer' />
-        <InputField id='phone' label='Phone' placeholder='+7 999 999 99 99' />
-        {/*<div className="inputs__wrapper">*/}
-        {/*  <label className="inputs__label" htmlFor="name">Name</label>*/}
-        {/*  <input className="inputs__input input" id="name" name="name" type="text" placeholder="Ivan"/>*/}
-        {/*</div>*/}
-
-        {/*<div className="inputs__wrapper">*/}
-        {/*  <label className="inputs__label" htmlFor="position">Position</label>*/}
-        {/*  <input className="inputs__input input" id="position" name="position" type="text" placeholder="Developer"/>*/}
-        {/*</div>*/}
-
-        {/*<div className="inputs__wrapper">*/}
-        {/*  <label className="inputs__label" htmlFor="phone">Phone</label>*/}
-        {/*  <input className="inputs__input input" id="phone" name="phone" type="text" placeholder="+7 999 999 99 99"/>*/}
-        {/*</div>*/}
+        <InputField id='name' label='Name' type='text' placeholder='Ivan' />
+        <InputField id='position' label='Position' type='text' placeholder='Developer' />
+        <InputField id='phone' label='Phone' type='phone' placeholder='+7 999 999 99 99' />
       </div>
 
       <p className="form__error" aria-live="assertive"></p>
