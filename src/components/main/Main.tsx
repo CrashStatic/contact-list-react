c53eea497d5c6a9a98149d0550e52b5bfa100674
CrashStatic <thecrashstatic@gmail.com>
2025-02-18 21:11:20 +0300

    Добавляет запись в localStorage

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -3,10 +3,23 @@ import ContactTable from "../contacTable/ContactTable";
 import {ALPHABET_A_M, ALPHABET_N_Z} from "../../alphabet/Alphabet";
 import './Main.css';
 import {Contact} from "../addContactForm/AddContactForm";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 
 export default function Main() {
   const [contacts, setContacts] = useState<Contact[]>([]);
+
+  useEffect(() => {
+    const savedContacts: string | null = localStorage.getItem("contacts");
+    if (savedContacts) {
+      setContacts(JSON.parse(savedContacts));
+    }
+  }, []);
+
+  useEffect(() => {
+    if (contacts.length > 0) {
+      localStorage.setItem("contacts", JSON.stringify(contacts));
+    }
+  }, [contacts]);
 
   function handleAddContact(newContact: Contact) {
     setContacts(prevContacts => [...prevContacts, newContact]);
