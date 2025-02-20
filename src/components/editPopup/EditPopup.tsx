64a3e9c38592474c867774052167b90a27bb5893
CrashStatic <thecrashstatic@gmail.com>
2025-02-20 22:27:49 +0300

    Настраивает правильное поведение для попапа редактирования

--- src/components/editPopup/EditPopup.tsx
+++ src/components/editPopup/EditPopup.tsx
@@ -1,10 +1,11 @@
 import InputField from "../../UI/input/InputField.js";
-import React, {useState} from "react";
+import React, {useEffect, useRef, useState} from "react";
 import Button from "../../UI/button/Button.js";
 import ModalHeader from "../modal/ModalHeader";
 import './EditPopup.css';
 import {Contact} from "../addContactForm/AddContactForm";
 import {validateForm} from "../../validate/validate";
+import {createPortal} from "react-dom";
 
 interface EditPopupProps {
   contact: Contact;
@@ -19,6 +20,7 @@ export default function EditPopup({contact, onSave, onClose, contacts}: EditPopu
   const [phone, setPhone] = useState(contact.phone);
   const [errors, setErrors] = useState<{ [key: string]: string }>({});
   const [currentError, setCurrentError] = useState<string | null>(null);
+  const nameInputRef = useRef<HTMLInputElement>(null);
 
   function handleSave(e: React.FormEvent) {
     e.preventDefault();
@@ -50,12 +52,36 @@ export default function EditPopup({contact, onSave, onClose, contacts}: EditPopu
     return `input${errors[field] ? " input--error" : ""}`;
   };
 
-  function handleClickInsidePopup(e: React.MouseEvent) {
-    e.stopPropagation();
+  function handleClickOutsidePopup(e: React.MouseEvent) {
+    if (e.target === e.currentTarget) {
+      onClose();
+    }
   }
 
-  return (
-    <dialog className="modal" open onClick={handleClickInsidePopup}>
+  function handleEscKeyDown(e: KeyboardEvent) {
+    if (e.key === "Escape") {
+      onClose();
+    }
+  }
+
+  useEffect(() => {
+    const handleEsc = (e: KeyboardEvent) => handleEscKeyDown(e);
+
+    document.addEventListener("keydown", handleEsc as EventListener);
+
+    return () => {
+      document.removeEventListener('keydown', handleEsc as EventListener)
+    }
+  }, [])
+
+  useEffect(() => {
+    if (nameInputRef.current) {
+      nameInputRef.current.focus();
+    }
+  }, []);
+
+  return createPortal(
+    <dialog className="modal" open onClick={handleClickOutsidePopup} onKeyDown={e => e.stopPropagation()}>
       <div className="modal__container">
         <ModalHeader onClose={onClose}/>
         <div className="modal__body" id="modal-body">
@@ -74,6 +100,7 @@ export default function EditPopup({contact, onSave, onClose, contacts}: EditPopu
                 value={name}
                 onChange={e => setName(e.target.value)}
                 className={getInputClassName('name')}
+                ref={nameInputRef}
               />
               <InputField
                 id='popup-position'
@@ -98,6 +125,7 @@ export default function EditPopup({contact, onSave, onClose, contacts}: EditPopu
           </form>
         </div>
       </div>
-    </dialog>
+    </dialog>,
+    document.getElementById('modal-popup')!
   )
 }
